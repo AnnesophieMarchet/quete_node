91bd04757f8173c5cc228773a01ca8ae6e3da70f
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-03-18 15:21:50 +0100

    commit quete node

---
+++ index.js
@@ -0,0 +1,16 @@
+const cowsay = require("cowsay");
+
+console.log(
+  cowsay.say({
+    text: "I'm a Coco",
+    e: "oO",
+    T: "U ",
+    ame: "Anne-so",
+  })
+);
+
+const coucou = require("./information");
+
+console.log(coucou.ame);
+
+console.log();
---
+++ information.js
@@ -0,0 +1,6 @@
+const objet = {
+  ame: "Anne-so",
+  age: 26,
+};
+
+module.exports = objet;
